313621111f1b70a6449f128342b360ffbbeee607
Prasun Suwal <prasun.suwal@gmail.com>
2025-07-24 13:15:33 +0545

    accept the pdf format as well

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -12,11 +12,11 @@ const storage = multer.diskStorage({
 
 //File filter
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "application/pdf"];
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Only .jpeg, .jpg and .png formats are allowed"), false);
+    cb(new Error("Only .jpeg, .jpg, .png, and .pdf formats are allowed"), false);
   }
 };
 
